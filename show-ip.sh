90c576e633da3ef4d84fd1a0db76ef27722bfde0
starling <smc@1kmi.co>
2022-11-25 09:36:37 -0800

    adjust awk to remove grep dependency

--- show-ip.sh
+++ show-ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ## title        :show-ip.sh
 ## description  :ip, mac and routing information with clean, minimal output
-## depends      :ip, grep, awk, tput, tabs
-## works in     :GNU bash version 4.3.11(1)
+## depends      :tput, tabs, echo, ip, awk, sort*
+## works in     :GNU bash version 4.3.11(1) and up.
 ## author       :starling
 ## date         :20150207
 ## notes        :This shows ip, mac, and routing information for all active
@@ -26,13 +26,13 @@ fi
 tabs 2
 
 echo -e "\nActive Interfaces:"
-ip -4 -d address show | grep -B1 -E "^  +inet " | \
-  awk ' { if ($1=="--" ) printf("\n"); else printf("%s ",$0); } ' | \
+ip -4 -d address show | \
   awk -v colm="${colormac}" -v coli="${colorip}" -v colr="${colorrst}" -v cold="${colordev}" \
-    '{
-       printf("  %s%-18s%s\tMAC: %s%s%s\t IP: %s%s%s\n", cold,$NF,colr,colm,$2,colr,coli,$20,colr ); 
+    '{ 
+      if ( $3 ~ /UP/   ) { printf("  %s%-18s%s\t", cold, substr($2,1, length($2)-1),colr) }
+      if ( $1 ~ /link/ ) { printf("MAC: %s%s%s\t",colm,$2,colr) }
+      if ( $1 ~ /inet/ ) { printf("IP: %s%s%s\n",coli,$2,colr) } 
      }'
-  # first awk concats the 2 lines from grep -B1
    
 echo -e "\nRouting:"
 ip -4 route show | sort --key=3.6 | awk -v cold="${colordev}" -v coli="${colorip}" -v colr="${colorrst}" \
@@ -41,15 +41,19 @@ ip -4 route show | sort --key=3.6 | awk -v cold="${colordev}" -v coli="${colorip
    } END { printf("\n"); }'  
 
 
-#  output sampe of "ip address show | grep -B1 inet"  
+#  as of 1.5 awk relies on the output of the ip command directly.
+#    1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+#    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00 promiscuity 0 minmtu 0 maxmtu 0 numtxqueues 1 numrxqueues 1 gso_max_size 65536 gso_max_segs 65535 
+#    inet 127.0.0.1/8 scope host lo
+#       valid_lft forever preferred_lft forever
 #
-#      link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-#      inet 127.0.0.1/8 scope host lo
-#      --
-#
-#  awk line ' { if ($1=="--" ) printf("\n"); else printf("%s ",$0); } '
-#  after the pipe through awk -- becomes a newline and the 2 lines above become one
-#      link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00  inet 127.0.0.1/8 scope host lo
+#  awk keys off "UP" in the <> to get the interface, "link" to get the mac,
+#  and "inet" to get the IP. These happen to be in the order I was already
+#  using. As long as the output from ip command is stable we should be good
+#  
 #  v1.2, changed interface grep to exclude ipv6 information.
 #  v1.3, changed ip command to only show ipv4 and adjusted awk to parse 
-#  v1.4, adjusted for shellcheck output (quoted variables in awk arg)    
+#  v1.4, adjusted for shellcheck output (quoted variables in awk arg) 
+#  v1.5, removed grep and redundant awk, called out sort as a dep, been there forever.
+#        sort keeps the routes grouped by interface/ip. It could be removed without
+#        impacting the function of the script. //20221125
